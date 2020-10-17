8ee521ecfa1c8244db1c53c41e1e81e92f672f0b
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-10-18 07:34:33 +0800

    Withdrawal action

--- app/controllers/debits_controller.rb
+++ app/controllers/debits_controller.rb
@@ -1,4 +1,19 @@
 class DebitsController < ApplicationController
+  before_action :set_person
+
   def create
+    debit = Debit.new(amount: debit_params[:amount], transact_to: @person)
+
+    if debit.save
+      flash[:success] = "MYR #{debit_params[:amount]} has been withdrawn!"
+    else
+      flash[:fail] = "Something went wrong please try again later"
+    end
+    redirect_to dashboard_path
+  end
+
+  private
+  def debit_params
+    params.permit(:amount)
   end
 end
