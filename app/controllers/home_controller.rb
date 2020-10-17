34fd7c6b5f7ef14607cfc264152afa95f998b1bf
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-10-17 13:40:54 +0800

    Redirect if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_action :redirect_if_logged_in
+
   def index
+  end
+
+  private
+  def redirect_if_logged_in
+    redirect_to dashboard_path if session[:user_id]
   end
 end
