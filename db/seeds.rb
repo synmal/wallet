c0afedf3c28024ce44deed2f2e49fc2e20523524
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-10-17 12:35:38 +0800

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do
+  User.create(username: Faker::Internet.username)
+end
+
+3.times do
+  Team.create(name: Faker::Internet.username)
+end
+
+3.times do
+  Stock.create(name: Faker::Internet.username)
+end
