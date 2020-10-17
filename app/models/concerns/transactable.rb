2ad03250518ac520f9d86bac01c68ef97ce6de2a
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-10-17 12:14:16 +0800

    Temp remove bootstrap

--- app/models/concerns/transactable.rb
+++ app/models/concerns/transactable.rb
@@ -5,7 +5,6 @@ module Transactable
     # has_many :wallet_transactions, as: :transact_from
     # has_many :wallet_transactions, as: :transact_to
 
-    has_many :transfers, as: :transact_from
     has_many :credits, as: :transact_from
     has_many :debits, as: :transact_to
   end
