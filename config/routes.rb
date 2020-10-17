85dca19a1916d2fd78a2cb8cd4a8920d6022821c
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-10-17 13:39:06 +0800

    Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Sessions
   resource :sessions, only: [:create, :destroy]
+
+  # Dashbaord
+  resource :dashboard, only: [:show], controller: 'dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
