ea655a9438c6d1670952005bfa6dec40022b4e9a
Malcolm Jeremiah <malcolmjeremiahrajeli@gmail.com>
2020-10-18 08:16:36 +0800

    Add session store for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.session_store :cookie_store, key: '_wallet_prod'
 end
